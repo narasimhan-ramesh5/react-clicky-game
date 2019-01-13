5a6d71d32161aa2bc2d21bac3486849aeba50673
Narasimhan Ramesh <narasimhan.ramesh5@gmail.com>
2019-01-13 14:09:02 -0800

    Updates

---
+++ precache-manifest.21b0069e46912b522699e50f4406e6e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55abe30a270b3be75741",
+    "url": "/react-clicky-game/static/js/runtime~main.55abe30a.js"
+  },
+  {
+    "revision": "d00c2ab7a50d6c522f0a",
+    "url": "/react-clicky-game/static/js/main.d00c2ab7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-clicky-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "d00c2ab7a50d6c522f0a",
+    "url": "/react-clicky-game/static/css/main.5d40d309.chunk.css"
+  },
+  {
+    "revision": "41d79dbb775bd5df79aae11383f57347",
+    "url": "/react-clicky-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-clicky-hame/precache-manifest.84ed46beb9404583f9ef7c4cf4fbffee.js"
+  "/react-clicky-game/precache-manifest.21b0069e46912b522699e50f4406e6e5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-clicky-hame/index.html", {
+workbox.routing.registerNavigationRoute("/react-clicky-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
